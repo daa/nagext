ce07373926b7cb8fa170fbf951eb18483e2a41e0
daa <adfrom2003@mail.ru>
2010-03-15 13:43:03 +0300

    Added license text

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from distutils.core import setup
 setup(name='python-nagext',
     version='0.1',
     description='Python interface to Nagios external commands',
+    long_description='This module allows sending external commands to Nagios from Python',
     license='LGPL3',
     author='Alexander Duryagin',
     author_email='daa@vologda.ru',
